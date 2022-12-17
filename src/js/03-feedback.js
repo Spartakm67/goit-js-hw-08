e69ab76a147637b89137ef34ab31a3f2a4eaae3b
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2022-12-17 23:13:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,9 +1,49 @@
 import throttle from 'lodash.throttle';
 
-const feedbackForm = document.querySelector('.feedback-form');
+const form = document.querySelector('.feedback-form');
 
 const FEEDBACK_KEY = "feedback-form-state";
+form.addEventListener('input', throttle(saveFeedbackMessage, 500));
+form.addEventListener('submit', resetFormAndStorage);
 
-feedbackForm.addEventListener('input', foo1);
-feedbackForm.addEventListener('submit', throttle((foo2), 500));
+let formData = {};
 
+loadPage();
+
+formDataAddStorage = () => localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
+
+function saveFeedbackMessage(event) {
+    event.preventDefault();
+
+      const {
+        elements: { email, message }
+    } = event.currentTarget;
+    
+    formData.email = email.value;
+    formData.message = message.value;
+    // console.log(formData);
+    
+    formDataAddStorage();
+    
+    // console.log(localStorage);
+};
+
+function loadPage() {
+  const storageValue = JSON.parse(localStorage.getItem(FEEDBACK_KEY));
+  if (storageValue) {
+    // console.log(storageValue);
+    form.elements.email.value = storageValue.email;
+    form.elements.message.value = storageValue.message;
+  }
+};
+
+function resetFormAndStorage(event) { 
+    event.preventDefault();
+    
+    if (form.email.value || form.message.value) {
+        console.log(formData);
+
+        form.reset();
+        localStorage.removeItem(FEEDBACK_KEY);
+    }
+   };
