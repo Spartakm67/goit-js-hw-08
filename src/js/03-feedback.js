5ec376f84f3916f0ce0101eabfa1f777b41b40b8
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2022-12-17 17:19:56 +0200

    Update 03-feedback.js
    Updeting Started

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,9 @@
+import throttle from 'lodash.throttle';
+
+const feedbackForm = document.querySelector('.feedback-form');
+
+const FEEDBACK_KEY = "feedback-form-state";
+
+feedbackForm.addEventListener('input', foo1);
+feedbackForm.addEventListener('submit', throttle((foo2), 500));
 
