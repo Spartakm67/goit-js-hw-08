aa9643aea9f3dd53f044483c33a3e9d042420275
Spartak Maznytskyi <109909363+Spartakm67@users.noreply.github.com>
2022-12-17 15:53:59 +0200

    Task 2 Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const PLAYBACK_TIME  = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(({ seconds }) => {
+    console.log(seconds);
+    localStorage.setItem(PLAYBACK_TIME, seconds);
+}, 1000));
+
+try {
+  player.setCurrentTime(JSON.parse(localStorage.getItem(PLAYBACK_TIME)));
+} catch (error) {
+  player.setCurrentTime(0);
+};
 
